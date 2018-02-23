90b36591ae9f66f83add7ced05a4428f93121417
Talos Electronics <desarrollo@taloselectronics.com>
2018-02-23 11:53:20 -0600

    upgrade

--- Evasor_Obstaculos2.0/Evasor_Obstaculos2.0.ino
+++ Evasor_Obstaculos2.0/Evasor_Obstaculos2.0.ino
@@ -1,3 +1,9 @@
+/*
+ * Talos Electronics
+ * Ing. Rafael Lozano Rolon 
+ * Modificado por ultima vez 23 Febrero 2018
+ */
+  
 #include "Evasor.h"
 Evasor Robot_Evasor(13,12,11,10,9,8,7,6,5);
 void setup()
